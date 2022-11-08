fe4e3303e79f560b1f09ec1f6f34124bacd3fc32
Akam Programist <akadev404@gmail.com>
2022-11-08 14:03:12 +0300

    first commit

--- crud-app/server.js
+++ crud-app/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const path = require("path");
 
 const app = express();
 
@@ -10,8 +12,23 @@ const PORT = process.env.PORT || 8080;
 //log requests
 app.use(morgan("tiny"));
 
+// parse request to body-parser
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// set view engine
+app.set("view engine", "ejs");
+
+// to add ejs files in a different folder but for now they are in views folder
+// app.set("views", path.resolve(__dirname, "views/ejs"));
+
+// load assets
+app.use("/css", express.static(path.resolve(__dirname, "assets/css")));
+app.use("/img", express.static(path.resolve(__dirname, "assets/img")));
+app.use("/js", express.static(path.resolve(__dirname, "assets/js")));
+// css / style.css;
+
 app.get("/", (req, res) => {
-  res.send("crud application");
+  res.render("index");
 });
 
 app.listen(PORT, () => {
