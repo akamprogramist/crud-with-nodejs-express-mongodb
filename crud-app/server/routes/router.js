2bd34697c6cd1f2c9bddbbc1a50188b10afcbf67
Akam Programist <akadev404@gmail.com>
2022-11-13 10:18:54 +0300

    commits

--- crud-app/server/routes/router.js
+++ crud-app/server/routes/router.js
@@ -23,6 +23,7 @@ route.get("/update-user", services.update_user);
 // API
 route.post("/api/users", controller.create);
 route.get("/api/users", controller.find);
+// url parameter in express
 route.put("/api/users/:id", controller.update);
 route.delete("/api/users/:id", controller.delete);
 
